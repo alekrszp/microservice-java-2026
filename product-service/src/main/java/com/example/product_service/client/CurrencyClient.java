d5d9a9c58f155db6011fdfcdb9858df9f34ac91c
alekrszp <alessandrodarosa.39@gmail.com>
2026-05-22 17:56:40 -0300

    Refactor CurrencyClient to use CotacaoResponse

--- product-service/src/main/java/com/example/product_service/client/CurrencyClient.java
+++ product-service/src/main/java/com/example/product_service/client/CurrencyClient.java
@@ -1,17 +1,16 @@
 package com.example.product_service.client;
 
+import com.example.product_service.model.CotacaoResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(
-        name = "currency-service",
-        fallback = CurrencyClientFallback.class
+    name = "currency-service",
+    fallback = CurrencyClientFallback.class
 )
 public interface CurrencyClient {
 
-    @GetMapping("/currency")
-    String getCurrency(
-            @RequestParam String moeda
-    );
+    @GetMapping("/currency/{moeda}")
+    CotacaoResponse getCotacao(@PathVariable("moeda") String moeda);
 }
